05a3955f6966628a3e1b91df5f9987425c694010
Rogiel <rogiel.silva@usp.br>
2016-04-24 11:17:10 -0300

    Com quicksort funcionando

--- BWT.c
+++ BWT.c
@@ -8,88 +8,92 @@ void preencheT_R(char *vector_T,char *aux_vector_T,int *vector_R, char *string, 
 	for (i = 0; i < N; i++)
 	{	
 		vector_T[i] = aux_vector_T[i] = string[i];
-
-		vector_R[i] = i;
+		vector_R[i] = i; // vetor de indices
 	}
 }
 
+void trocaPosicao(char *vector_T, int *vector_R, int i, int j)
+{
+	int temp;
 
-void selectionSort(char *vector_T, char *aux_vector_T, int *vector_R, int N) {
+	temp = vector_T[j];
+	vector_T[j] = vector_T[i];
+	vector_T[i] = temp;
 
-	int i, j, minIndex, tmp;    
-	int temp2,auxCompI, auxCompJ,somaI,somaJ;
-
-	for (i = 0; i < N-1; i++) {
-
-		minIndex = i;
-		for (j = i + 1; j < N; j++){
-			auxCompI = auxCompJ = 0;
-			if (vector_T[j] <= vector_T[minIndex]){
-				if (vector_T[j] == vector_T[minIndex]){
-					somaI = vector_R[j] + auxCompI;
-					somaJ = vector_R[minIndex] + auxCompJ;					
-
-					while (auxCompI < N-1 || auxCompJ < N-1)
-					{	
-						if (somaI > N)
-						{	
-							somaI = 0;
-							auxCompI = 0;
-							somaJ = vector_R[minIndex] + auxCompJ +1;
-
-						}
-						if (somaJ > N)
-						{
-							somaJ = 0 ;
-							auxCompJ=0;
-							somaI = vector_R[j] + auxCompI+1;
-
-						}
-						if(aux_vector_T[somaI] == aux_vector_T[somaJ])
-						{
-
-						} //continue if equal
-						else if (aux_vector_T[somaI] < aux_vector_T[somaJ])
-						{
-
-							minIndex = j;
-							break;
-						}
-						else break;
-						
-						auxCompI++;
-						auxCompJ++;
-						somaI = somaI + auxCompI;
-						somaJ = somaJ + auxCompJ;
-						 
-						}
-				
-					}else minIndex = j;
-					
-				}
-
-			} 
-			if (minIndex != i) 
-			{
-
-				tmp = vector_T[i];
-				vector_T[i] = vector_T[minIndex];
-				vector_T[minIndex] = tmp;
-
-				temp2 = vector_R[i];
-				vector_R[i] = vector_R[minIndex];
-				vector_R[minIndex] = temp2;
-
-
-
-			}
-		}
+	temp = vector_R[j];
+	vector_R[j] = vector_R[i];
+	vector_R[i] = temp;
 }
 
-void pegaLinhaF(char *string_rotate, char *vector_T, int N)
+int partition(char *vector_T, char *aux_vector_T, int *vector_R,int min, int N, int TAM)
+{
+	
+	int x, xR,i;
+
+	int j = min - 1;
+	int somaI,somaX;
+
+	x = vector_T[N];
+	xR = vector_R[N];
+
+	for (i = min; i < N; i++) {
+
+		if (x >= vector_T[i])
+		{			
+			if (x == vector_T[i]){ 
+				somaX = xR + 1;
+				somaI = vector_R[i] + 1 ;
+
+				while (1)  // para tratar os caso de desempate, comparando cada caracter até ser difirente
+				{	
+					if (somaX >= TAM) somaX = 0;	 // caso passe do tamanho do vetor, volta para o inicio
+					if (somaI >= TAM) somaI = 0;					
+
+					if(aux_vector_T[somaX] == aux_vector_T[somaI]); // caso seja igual continua a comparar												
+					else if (aux_vector_T[somaX] > aux_vector_T[somaI])
+					{
+						j = j + 1;
+						trocaPosicao(vector_T,vector_R, i,j); // para trocar as posições dos vetores
+						break;
+					}
+					else break;
+					
+					somaX +=1;
+					somaI +=1;
+					 
+				}			
+			}
+			else
+			{
+				j = j + 1;
+				trocaPosicao(vector_T,vector_R, i,j);
+			} 
+		}
+	}
+
+	vector_T[N] = vector_T[j + 1];
+	vector_T[j + 1] = x;
+
+	vector_R[N] = vector_R[j + 1];
+	vector_R[j + 1] = xR;
+	return (j + 1);
+}
+
+void quickSort(char *vector_T, char *aux_vector_T, int *vector_R, int min, int N, int TAM)
+{ 
+	int q;
+	
+	if (min < N) 
+	{
+		q = partition(vector_T,aux_vector_T,vector_R, min,N, TAM);
+		quickSort(vector_T,aux_vector_T,vector_R,min, q-1, TAM);
+		quickSort(vector_T,aux_vector_T, vector_R, q+1, N, TAM);
+	}
+}
+void pegaLinhaF(char *string_rotate, char *vector_T, int N) // pega a ultima linha, no caso é a que será codificada
 {
 	int i;
-	string_rotate[0] = vector_T[N-1];
+	string_rotate[0] = vector_T[N-1]; 
 	for (i=1; i < N; i++) string_rotate[i] = vector_T[i-1];
 }
 
@@ -113,27 +117,11 @@ int main()
 
 	preencheT_R(vector_T,aux_vector_T,vector_R,string,N); // N is the name de bloco that need read.
 	pegaLinhaF(string_rotate,vector_T,N);
-	selectionSort(vector_T, aux_vector_T,vector_R,N);
-	
+	quickSort(vector_T, aux_vector_T,vector_R,0,N-1,N);
 
-
-	printf("\n\n");
-	for (i = 0; i < N; i++)
-	{
-		printf("%c -> %d\n",vector_T[i], vector_R[i]);
-		
-		
-	}
-	printf("\n\n");
-	for (i = 0;i < N; i++)
-	{
-		printf("%c -> %d\n",string_rotate[vector_R[i]], vector_R[i]);
-		
-		
-	}
+	for (i = 0;i < N; i++)	printf("%c -> %d\n",string_rotate[vector_R[i]], vector_R[i]);
 	printf("\n\n");
 
-	free(string);
 
 	/*
 	FILE *arq;
@@ -157,8 +145,12 @@ int main()
   
 
 	fclose (arq);*/
-  free(vector_T);
-  free(vector_R);
+  	free(vector_T);
+  	free(vector_R);
+  	free(string);
+  	free(aux_vector_T);
+	free (string_rotate);
+ 
 return 0;
 }
 
